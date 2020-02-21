d4c1f0e166371f57502261a922eab6a8c8f08459
Kirill Stoianov <kirillstoianov46@gmail.com>
2020-02-21 10:37:34 +0200

    Initial commit

--- src/main/java/com/haha/Application.java
+++ src/main/java/com/haha/Application.java
@@ -1,7 +1,10 @@
 package com.haha;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        new Launcher().launch(args);
+        new Launcher().launch(new ArrayList<>(Arrays.asList(args)));
     }
 }
--- src/main/java/com/haha/Launcher.kt
+++ src/main/java/com/haha/Launcher.kt
@@ -1,22 +1,45 @@
 package com.haha
 
-import java.nio.file.Files
-import java.util.*
-import kotlin.collections.ArrayList
+import java.io.File
 
 class Launcher {
-    fun launch(args: Array<String>) {
-        println("Args: ${args.contentToString()}")
+    fun launch(args: ArrayList<String>) {
+        println("Args: $args")
+
         val fileUtil = FileUtil()
-        val launchDirectory = fileUtil.getJarLaunchDirectory()
+
+
+        var launchDirectory: File =File("/Users/kirillstoianov/Android/Projects/real-android/app/src")// fileUtil.getJarLaunchDirectory()
+        var fileExtension: ArrayList<String> = arrayListOf(".java")
+
+        when {
+            args.contains("-f") -> {
+                val filePath = args.getOrNull(args.indexOf("-f") + 1)
+                if (filePath != null) {
+                    launchDirectory = File(filePath)
+                }
+            }
+
+            args.contains("-ext") -> {
+                args.getOrNull(args.indexOf("-ext") + 1)?.apply { fileExtension.add(this) }
+            }
+        }
+
+
         println("\n\n-----------------------------------------")
         println("Launch directory: ${launchDirectory.path}")
 
-        fileUtil.getFilesList(launchDirectory).filter { it.isFile }.forEach {
-
-            if (fileUtil.getFileExtension(it) == ".kt") {
-                it.useLines { it.toList() }.forEach { println(it) }
+        var filesCount = 0
+        val lineCount = fileUtil.getFilesList(launchDirectory)
+            .filter { it.isFile }
+            .filter { fileExtension.contains(fileUtil.getFileExtension(it))}
+            .map {
+                filesCount += 1
+                return@map it.useLines { line -> line.toList() }.size
             }
-        }
+            .sumBy { it }
+
+        println("Files count = $filesCount")
+        println("Line count = $lineCount")
     }
 }
